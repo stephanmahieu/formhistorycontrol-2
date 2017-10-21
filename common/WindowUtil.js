203a87e0ecedc4e4bcd2f587ce82eb82269c1149
Stephan <stephanmahieu@yahoo.com>
2017-10-21 17:46:37 +0200

    Added modal dialogs

--- common/WindowUtil.js
+++ common/WindowUtil.js
@@ -110,6 +110,102 @@ class WindowUtil {
         });
     }
 
+    static showModalInformation(titleId, messageId) {
+        WindowUtil.showModalDialog(titleId, messageId, 'modal-information');
+    }
+    static showModalWarning(titleId, messageId) {
+        WindowUtil.showModalDialog(titleId, messageId, 'modal-warning');
+    }
+    static showModalError(titleId, messageId) {
+        WindowUtil.showModalDialog(titleId, messageId, 'modal-error');
+    }
+    static showModalYesNo(titleId, messageId) {
+        WindowUtil.showModalDialog(titleId, messageId, 'modal-question', 'YesNo');
+    }
+
+    static showModalDialog(titleId, messageId, iconClass, buttons) {
+        const dlgTitle = document.createElement('span');
+        dlgTitle.classList.add('modal-title');
+        dlgTitle.appendChild(document.createTextNode(browser.i18n.getMessage(titleId)));
+
+        const dlgClose = document.createElement('span');
+        dlgClose.classList.add('modal-close', 'modal-button');
+
+        const dlgHeader = document.createElement('div');
+        dlgHeader.classList.add('modal-header');
+        dlgHeader.appendChild(dlgTitle);
+        dlgHeader.appendChild(dlgClose);
+
+        const dlgIcon = document.createElement('div');
+        dlgIcon.classList.add('modal-icon', iconClass);
+
+        const dlgText = document.createElement('div');
+        dlgText.classList.add('modal-message');
+        dlgText.appendChild(document.createTextNode(browser.i18n.getMessage(messageId)));
+
+        const dlgContent = document.createElement('div');
+        dlgContent.classList.add('modal-content');
+        dlgContent.appendChild(dlgHeader);
+        dlgContent.appendChild(dlgIcon);
+        dlgContent.appendChild(dlgText);
+
+        const dialog = document.createElement('div');
+        dialog.classList.add('modal');
+        dialog.appendChild(dlgContent);
+
+        if (buttons) {
+            const dlgButtons = document.createElement('div');
+            dlgButtons.classList.add('modal-buttons');
+            if (~buttons.toLowerCase().indexOf('no')) {
+                dlgButtons.appendChild(WindowUtil._createDialogButton('buttonNo', 'modal-no'));
+            }
+            if (~buttons.toLowerCase().indexOf('cancel')) {
+                dlgButtons.appendChild(WindowUtil._createDialogButton('buttonCancel', 'modal-cancel'));
+            }
+            if (~buttons.toLowerCase().indexOf('yes')) {
+                dlgButtons.appendChild(WindowUtil._createDialogButton('buttonYes', 'modal-yes'));
+            }
+            if (~buttons.toLowerCase().indexOf('okay')) {
+                dlgButtons.appendChild(WindowUtil._createDialogButton('buttonOkay', 'modal-ok'));
+            }
+            dlgContent.appendChild(dlgButtons);
+        }
+
+        document.body.appendChild(dialog);
+        if (!buttons) {
+            dlgClose.addEventListener('click', WindowUtil.removeModalDialog);
+            dialog.addEventListener('click', WindowUtil.removeModalDialog);
+        } else  {
+            return new Promise(function(resolve, reject) {
+                Array.from(document.getElementsByClassName('modal-button')).forEach(elem => {
+                    elem.addEventListener('click', event => {
+                        let isYes = event.target.classList.contains('modal-yes');
+                        let isOkay = event.target.classList.contains('modal-ok');
+                        WindowUtil.removeModalDialog();
+                        if (isYes || isOkay) {
+                            resolve(isYes?'yes':'ok');
+                        } else {
+                            reject('modal dialog: user cancelled');
+                        }
+                    });
+                });
+            });
+        }
+    }
+
+    static removeModalDialog() {
+        const dialog = document.getElementsByClassName('modal').item(0);
+        dialog.removeEventListener('click', WindowUtil.removeModalDialog);
+        document.body.removeChild(dialog);
+    }
+
+    static _createDialogButton(i18nNameId, classname) {
+        const dlgButton = document.createElement('button');
+        dlgButton.appendChild(document.createTextNode(browser.i18n.getMessage(i18nNameId)));
+        dlgButton.classList.add(classname, 'modal-button');
+        return dlgButton;
+    }
+
     // snippet:
     // // add a listener once in order to close this popup if it loses focus
     // if (!currentIdWindowAboutFHC) {
